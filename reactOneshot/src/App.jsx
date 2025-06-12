d102d83e962a2ab4070086221bf353934561e904
naman <namankhetwal99@gmail.com>
2025-06-13 01:03:53 +0530

    Implement user state management with hooks and add functionality to change user and value

--- reactOneshot/src/App.jsx
+++ reactOneshot/src/App.jsx
@@ -1,12 +1,29 @@
-// JSX
-import React from 'react'
+// Hooks - special type of functions 
+import React, {useState} from 'react'
 const App = () =>{
-  const user ="naman"
+
+  // let user ="naman"
+
+  const [user , nameChange] = useState('naman')
+  const [a , setA] = useState(10)
+
+  const changeUser =()=>{
+    nameChange("mohit")
+  }
+
+  const changeA = () =>{
+    setA(20)
+  }
+
   return(
     <div>
      <h1>Username is {user}</h1>
      {/* {} bracker are used to a call a parameter or a variable in the function  */}
-     <button>Change User</button>
+     <button onClick={changeUser}>Change User</button>
+
+
+     <h1>Value of a is {a}</h1>
+     <button onClick={changeA}>Change A </button>
     </div>
     
   )
