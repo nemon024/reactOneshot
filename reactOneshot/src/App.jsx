d1b6e33ecb5ca6c3094e96e42f5a5f4df77053c7
naman <namankhetwal99@gmail.com>
2025-06-13 00:10:32 +0530

    Refactor App component to display username and add a button for user change

--- reactOneshot/src/App.jsx
+++ reactOneshot/src/App.jsx
@@ -1,8 +1,15 @@
-function App(){
-  return <div>
-    <h1>hello</h1>
-    <h2>not hello</h2>
-  </div>
+// JSX
+import React from 'react'
+const App = () =>{
+  const user ="naman"
+  return(
+    <div>
+     <h1>Username is {user}</h1>
+     {/* {} bracker are used to a call a parameter or a variable in the function  */}
+     <button>Change User</button>
+    </div>
+    
+  )
 }
 
 export default App
