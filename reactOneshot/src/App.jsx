321e705d99437c98567d2d2de9d67da73d881083
naman <namankhetwal99@gmail.com>
2025-06-13 23:58:29 +0530

    two way binding

--- reactOneshot/src/App.jsx
+++ reactOneshot/src/App.jsx
@@ -1,20 +1,25 @@
-// Hooks - special type of functions 
-import React, {useState} from 'react'
+import React,{useState} from 'react'
 const App = () =>{
 
- const [num , setNum] = useState(0)
-
- const submitHandler = (e) =>{
-  e.preventDefault()
-  console.log("submitted")
- }
-
+  const [username , setUsername] = useState('')
+  const submitHandler = (e) =>{
+    e.preventDefault()
+    console.log(username)
+    setUsername('')
+  }
   return(
     <div>
-      <form action="" onSubmit={(e) =>{
-        submitHandler(e)
-      }}>
-        <input className="px-4 py-3 text-xl m-5" type="" placeholder="Enter you name" />
+      <form onSubmit = {
+        (e)=>{
+          submitHandler(e)        
+        }
+      }>
+        <input 
+        value={username}
+        onChange = {(e)=>{
+          setUsername(e.target.value)
+        }}
+        className="px-4 py-3 text-xl m-5" type="text" placeholder="Enter you name" />
         <button className="px-4 m-5 py-3 text-xl font-semibold bg-emerald-600 rounded text-white">Submit</button>
       </form>
     </div>  
