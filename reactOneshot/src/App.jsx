24c6773751a60522dc66711faa865acf8dadfb8f
naman <namankhetwal99@gmail.com>
2025-06-21 00:43:32 +0530

    learn about integrating APi(axios) and rendering JSON data

--- reactOneshot/src/App.jsx
+++ reactOneshot/src/App.jsx
@@ -1,11 +1,27 @@
-// import React from 'react'
-import Card from './components/Card.jsx';
-import Ex from './components/Ex.jsx';
+import axios from 'axios';
+import React,{useEffect , useState} from 'react'
 
 const App = () => {
+
+  const [data, setData] = useState([])
+
+  const getData = async()=>{
+    const response= await axios.get('https://picsum.photos/v2/list')
+    setData(response.data)
+  }
+
+  useEffect(()=>{
+    getData()
+  },[])
   return(
-    <div>
-      <Ex name="Naman"/>
+    <div className='p-10'>
+   
+    <div className='p-5 mt-5 bg-gray-950'>{data.map(function(elem ,idx){
+      return <div key={idx} className='bg-gray-50 text-black flex items-center justify-between w-full px-7 py-6 rounded mb-3'>
+        <img className="h-40" src={elem.download_url} alt=""/>
+        <h1>{elem.author}</h1>
+      </div>
+    })}</div>
     </div>
   )
 }
--- reactOneshot/src/components/Ex2.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const Ex2 = (props) =>{
-  return(
-    <div>Example2 {props.username}</div>
-  )
-}
-
-export default Ex2
